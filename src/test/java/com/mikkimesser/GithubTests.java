98f55e07e14e2b55ba0afa0201d21c51503a03a3
Mike Bulanov <mikki.messer@gmail.com>
2022-05-11 21:29:49 +0300

    allure listener added

--- src/test/java/com/mikkimesser/GithubTests.java
+++ src/test/java/com/mikkimesser/GithubTests.java
@@ -2,6 +2,8 @@ package com.mikkimesser;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -13,12 +15,13 @@ public class GithubTests {
     public final String pageURL = "https://github.com";
     public final String searchText = "eroshenkoam/allure-example";
     public final String tabHeader = "Issues";
-    public final String issueName = "с днем археолога!";
+    public final String issueName = "с днем археологга!";
 
     @BeforeAll
     public static void setUp() {
         Configuration.browserSize = "1280x720";
         Configuration.holdBrowserOpen = true;
+        SelenideLogger.addListener("allure", new AllureSelenide());
     }
 
     @Test
