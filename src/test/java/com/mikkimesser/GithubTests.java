61ce9c402115370a80a3613a9dc26dceb16cb2fc
Mike Bulanov <mikki.messer@gmail.com>
2022-05-11 22:19:10 +0300

    lambda steps added

--- src/test/java/com/mikkimesser/GithubTests.java
+++ src/test/java/com/mikkimesser/GithubTests.java
@@ -10,28 +10,61 @@ import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.$;
+import static io.qameta.allure.Allure.step;
+import static org.openqa.selenium.By.linkText;
+import static org.openqa.selenium.By.partialLinkText;
 
 public class GithubTests {
-    public final String pageURL = "https://github.com";
-    public final String searchText = "eroshenkoam/allure-example";
-    public final String tabHeader = "Issues";
-    public final String issueName = "с днем археологга!";
+    private final String pageURL = "https://github.com";
+    private final String repository = "eroshenkoam/allure-example";
+    private final String tabHeader = "Issues";
+    private final String issueName = "с днем археологга!";
+    private String issueNameNormalized = "";
 
     @BeforeAll
     public static void setUp() {
         Configuration.browserSize = "1280x720";
         Configuration.holdBrowserOpen = true;
-        SelenideLogger.addListener("allure", new AllureSelenide());
     }
 
     @Test
     public void checkIssuesSimple() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
         Selenide.open(pageURL);
-        $(".header-search-input").setValue(searchText).pressEnter();
-        $(By.linkText(searchText)).click();
-        $(By.partialLinkText(tabHeader)).click();
+        $(".header-search-input").setValue(repository).pressEnter();
+        $(linkText(repository)).click();
+        $(partialLinkText(tabHeader)).click();
         String issueNameNormalized = issueName.trim().replaceAll(" +", " ");
         System.out.println(issueNameNormalized);
         $(withText(issueNameNormalized)).click();
+    }
+
+    @Test
+    public void checkIssuesStepLambda() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Открываем страницы", ()->{
+            Selenide.open(pageURL);
+        });
+
+        step(String.format("%s %s", "ищем репозиторий", repository), ()->{
+            $(".header-search-input").setValue(repository).pressEnter();
+        });
+
+        step(String.format("%s %s", "открываем репозиторий", repository), ()-> {
+            $(linkText(repository)).click();
+        });
+
+        step(String.format("%s %s", "кликаем по табу",tabHeader), ()->{
+            $(partialLinkText(tabHeader)).click();
+        });
+
+        step(String.format("нормализуем название сущности %s для поиска", issueName), ()->{
+            issueNameNormalized = issueName.trim().replaceAll(" +", " ");
+        });
+
+        step(String.format("%s %s","кликаем по сущности под названием",issueName), ()->{
+            $(withText(issueNameNormalized)).click();
+        });
     }
 }
